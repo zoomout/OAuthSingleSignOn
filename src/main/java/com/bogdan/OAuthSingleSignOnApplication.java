6015381dae89e4126641f226c5a97ba0fddcf00f
zoomout <bogdan.zastavnyi@irdeto.com>
2016-11-24 23:26:44 +0100

    initiated spring boot project including maven dependencies for oauth app

---
+++ src/main/java/com/bogdan/OAuthSingleSignOnApplication.java
@@ -0,0 +1,12 @@
+package com.bogdan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthSingleSignOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthSingleSignOnApplication.class, args);
+	}
+}
