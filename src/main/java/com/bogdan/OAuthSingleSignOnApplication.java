4718d6314640e8ddd9fed2c3ee7a055fb81ded01
zoomout <bogdan.zastavnyi@irdeto.com>
2016-11-25 00:13:09 +0100

    added properties, index.html, user endpoing, http security configuration

--- src/main/java/com/bogdan/OAuthSingleSignOnApplication.java
+++ src/main/java/com/bogdan/OAuthSingleSignOnApplication.java
@@ -2,11 +2,41 @@ package com.bogdan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class OAuthSingleSignOnApplication {
+@EnableOAuth2Sso
+@RestController
+public class OAuthSingleSignOnApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OAuthSingleSignOnApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthSingleSignOnApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "login**", "webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll().and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
 }
